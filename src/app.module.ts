a0d3848edb4c393c7ecb39e69f389c4a2c9d9731
undefinedSahed <sahedrahmanshakil@gmail.com>
2026-01-15 21:11:51 +0600

    add: profile module and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProfilesModule } from './profiles/profiles.module';
+import { PorfilesController } from './porfiles/porfiles.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [ProfilesModule],
+  controllers: [AppController, PorfilesController],
   providers: [AppService],
 })
 export class AppModule {}

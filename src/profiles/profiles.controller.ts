184be1935814638ced0ca07a2590db8ba4441e73
undefinedSahed <sahedrahmanshakil@gmail.com>
2026-01-16 15:31:24 +0600

    add: get single profile

--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -1,10 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 
 @Controller('profiles')
 export class ProfilesController {
   // Get /profiles
   @Get()
-  findAll() {
-    return ['Sahed', 'John', 'Doe'];
+  findAll(@Query('age') age: number) {
+    return [{ age }];
+  }
+
+  // Get /profiles/id
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return [{ id }];
   }
 }

69c036e05f316163f4fe9f4b30c03a973e564ae8
undefinedSahed <sahedrahmanshakil@gmail.com>
2026-01-16 15:52:01 +0600

    add: update profile api

--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, Query } from '@nestjs/common';
 import { CreateProfileDto } from './dto/create-profile.dto';
+import { UpdateProfileDto } from './dto/update-profile.dto';
 
 @Controller('profiles')
 export class ProfilesController {
@@ -21,6 +22,18 @@ export class ProfilesController {
     return {
       name: createProfileDto.name,
       description: createProfileDto.description,
+    };
+  }
+
+  // Update /profile/:id
+  @Put(':id')
+  updateProfile(
+    @Param('id') id: string,
+    @Body() updateProfileDto: UpdateProfileDto,
+  ) {
+    return {
+      id,
+      ...updateProfileDto,
     };
   }
 }

814da6a184c83a1c0a36f87bfbd0dc44afb4bbe7
undefinedSahed <sahedrahmanshakil@gmail.com>
2026-01-16 15:46:12 +0600

    add: create profile api

--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { CreateProfileDto } from './dto/create-profile.dto';
 
 @Controller('profiles')
 export class ProfilesController {
@@ -12,5 +13,14 @@ export class ProfilesController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return [{ id }];
+  }
+
+  // Post /profiles
+  @Post()
+  createProfile(@Body() createProfileDto: CreateProfileDto) {
+    return {
+      name: createProfileDto.name,
+      description: createProfileDto.description,
+    };
   }
 }
